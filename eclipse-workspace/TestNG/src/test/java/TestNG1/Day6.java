4eabe0497505662f24ec0e3b6908ad24bee5e0dc
Sabina Dahal <SabinaDahal>
2023-10-04 13:20:47 -0500

    added

--- eclipse-workspace/TestNG/src/test/java/TestNG1/Day6.java
+++ eclipse-workspace/TestNG/src/test/java/TestNG1/Day6.java
@@ -77,7 +77,7 @@ public class Day6 {
 		
 	  driver.get("http://www.webdriveruniversity.com/"); 
 	  //driver.findElement(By.id("contact-us")).click();  
-	  WebElement scrollElement  = driver.findElement(By.id("scrolling-around")); 
+	//  WebElement scrollElement  = driver.findElement(By.id("scrolling-around")); 
 	  JavascriptExecutor je = (JavascriptExecutor) driver; 
 	  String title = (String)je.executeScript("return document.title;"); 
 	  // First parameter of  JavascriptExecutor is script 
@@ -98,5 +98,69 @@ public class Day6 {
 	  String className = e.getAttribute("class"); 
 	  Assert.assertEquals(className, "div-double-click double"); 
 	  driver.close();  
-	 } }
+	 } 
+	 
+	 
+		@Test
+		public void testcase6() throws InterruptedException {
+			
+			
+		ChromeDriver driver = new ChromeDriver();
+			
+			   driver.get("http://www.webdriveruniversity.com/Actions/index.html");
+		   Actions ac = new Actions(driver);
+		   WebElement draggable = driver.findElement(By.id("draggable"));
+		   WebElement droppable = driver.findElement(By.id("droppable"));
+		   ac.dragAndDrop(draggable, droppable).build().perform();
+		   Assert.assertEquals(droppable.getText(), "Dropped!");
+		   driver.close();
+		
+		}
+		
+		@Test
+		public void testcase7() throws InterruptedException {
+			
+				System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\sabin\\Downloads\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		   driver.get("http://www.webdriveruniversity.com/Actions/index.html");
+		   Actions ac = new Actions(driver);
+		   WebElement clickable = driver.findElement(By.id("click-box"));
+		   ac.clickAndHold(clickable).build().perform();
+		   Assert.assertEquals(clickable.getText(), "Well done! keep holding that click now.....");
+		   driver.close();
+		
+		}
+		
+
+		@Test
+		public void TestCase8() throws InterruptedException {
+			
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\sabin\\Downloads\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+			
+			driver.get("http://www.webdriveruniversity.com/Actions/index.html");
+			Actions ac = new Actions(driver);
+			WebElement e=driver.findElement(By.cssSelector("#double-click"));
+			ac.contextClick(e).build().perform();
+			Thread.sleep(5000);
+			driver.close();
+			}	
+		@Test
+		public void TestCase9() throws InterruptedException {
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\sabin\\Downloads\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+			 
+			driver.get("http://www.webdriveruniversity.com/Actions/index.html");
+			
+			
+			Actions ac = new Actions(driver);
+			WebElement e=driver.findElement(By.cssSelector("#div-hover > div.dropdown.hover > button"));
+			ac.moveToElement(e).build().perform();
+			WebElement e2=driver.findElement(By.cssSelector("#div-hover > div.dropdown.hover > div"));
+			Assert.assertEquals(true, e2.isDisplayed());
+		}}
 
